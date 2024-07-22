49f04263fc5f3f0854395f825ab2a22879f42229
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2024-07-22 21:28:21 +0600

    need to work on it more

--- AllProgramFiles/MaxNumber/MaxBetweenNumbers.cs
+++ AllProgramFiles/MaxNumber/MaxBetweenNumbers.cs
@@ -24,4 +24,30 @@ public class MaxBetweenNumber
             Console.WriteLine("Known Value");
         }
     }
+    public void getMaxThreeNumber()
+    {
+        // 1 > 2 1 > 1
+        Console.WriteLine("Enter First number: ");
+        int numberOne = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter second number: ");
+        int numberTwo = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter third number: ");
+        int numberThree = Convert.ToInt32(Console.ReadLine());
+        if(numberOne >= numberTwo)
+        {
+            if(numberOne >= numberThree)
+            {
+                Console.WriteLine($"First number is greater {numberOne}");
+            }
+            else if(numberTwo >= numberThree)
+            {
+                Console.WriteLine($"Second number is greater {numberTwo}");
+            }
+            else 
+            {
+                Console.WriteLine($"Third number is greater {numberThree}");
+            }
+        }
+        
+    }
 }
