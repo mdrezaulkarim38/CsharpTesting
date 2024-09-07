e2ee6bd481ab5e756372a1be3a9fa9bfd6e228b9
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2024-09-07 08:30:07 +0600

    New Data Type

---
+++ AllProgramFiles/DataType/DataType.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CsharpTesting.AllProgramFiles.DataType
+{
+    public class DataType
+    {
+        
+    }
+}
