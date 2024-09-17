18d25fb154cb3542c670c2505bfedaab6d346784
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2024-09-17 19:57:38 +0600

    as is done

--- Program.cs
+++ Program.cs
@@ -6,6 +6,9 @@ using CsharpTesting.AllProgramFiles.Pyramid; */
 //using CsharpTesting.AllProgramFiles.Sorting;
 using CsharpTesting.AllProgramFiles;
 //using CsharpTesting.AllProgramFiles.DataType;
+
+/* class Geeks1 { }
+class Geeks2 { } */
 public class Program
 {
     /* public static void AddStr(dynamic s1, dynamic s2)
@@ -76,14 +79,56 @@ public class Program
         Y obj2 = new Y();
         Console.WriteLine("Value of x is : {0}", obj2.getX()); */
 
-        int num = 2020;
+        /* int num = 2020;
         object obj = num;
         //num = 100;
         int i = (int)obj;
         //System.Console.WriteLine("Value - type value of num is : {0}", num);
         System.Console.WriteLine("Object - type value of obj is : {0}", obj);
-        System.Console.WriteLine("Object - type value of i is : {0}", i);
+        System.Console.WriteLine("Object - type value of i is : {0}", i); */
+
+        /* string str1 = "GFG";
+        object obj1 = str1;
+        string str2 = obj1 as string;
+        if(str2 != null)
+        {
+            Console.WriteLine("Successfully Cast");
+        }
+
+        List<string> myList = obj1 as List<string>;
+        if(myList != null)
+        {
+            Console.WriteLine("Successfully Cast");
+        }
+        else
+        {
+            Console.WriteLine("Not Successful");
+        }
         
+        object[] obj = new object[5];
+        obj[0] = new Geeks1();
+        obj[1] = new Geeks2();
+        obj[2] = "C#";
+        obj[3] = 334.5;
+        obj[4] = null;
+ 
+        for (int j = 0; j < obj.Length; ++j) {
+ 
+            // using as operator
+            string str = obj[j] as string;
+ 
+            Console.Write("{0}:", j);
+ 
+            // checking for the result
+            if (str != null) {
+                Console.WriteLine("'" + str + "'");
+            }
+            else {
+                Console.WriteLine("Is is not a string");
+            }
+        } */
+
+
 
     }
 }
