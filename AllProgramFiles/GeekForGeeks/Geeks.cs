88c9a2a8d8a701e2c15b4965d230004566f14c22
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2024-11-09 20:13:11 +0600

    update code

---
+++ AllProgramFiles/GeekForGeeks/Geeks.cs
@@ -0,0 +1,13 @@
+namespace CsharpTesting.AllProgramFiles.GeekforGeeks;
+class Geeks
+{
+    public string Name;
+    public string GetName()
+    {
+        return Name;
+    }
+    public void SetName(string Name)
+    {
+        this.Name = Name;
+    }
+}
