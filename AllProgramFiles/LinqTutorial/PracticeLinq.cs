941e5238ce43ee8e1f0d5c6482f356118555baf7
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2024-11-16 15:50:53 +0600

    first query

--- AllProgramFiles/LinqTutorial/PracticeLinq.cs
+++ AllProgramFiles/LinqTutorial/PracticeLinq.cs
@@ -8,7 +8,19 @@ public class PracticeLinq
 {
     public void DisplayData()
     {
-        Console.WriteLine("Shuvo");
+        List<string> list = new List<string>()
+        {
+              " Hii JavaTpoint",  
+            " Welcome to JavaTpoint ",  
+            " The topic is LINQ ",  
+            " Any Queries "
+        };
+
+        var r = from I in list where I.Contains("JavaTpoint") select I;
+        foreach(var i in r)
+        {
+            Console.WriteLine(i);
+        }
         
     }
         
