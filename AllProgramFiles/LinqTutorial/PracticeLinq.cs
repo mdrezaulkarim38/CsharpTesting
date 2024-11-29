4550412a6a78511a96e9267a8a952504b67b8894
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2024-11-29 09:25:35 +0600

    New file added to the file

--- AllProgramFiles/LinqTutorial/PracticeLinq.cs
+++ AllProgramFiles/LinqTutorial/PracticeLinq.cs
@@ -21,7 +21,23 @@ public class PracticeLinq
         {
             Console.WriteLine(i);
         }
-        
+
+        List<int> intList = new List<int>()
+        {
+            1,2,3,4,5,6,7,8,9,10,11,12
+        };
+
+        var queries = from i in intList where i > 5 select i;
+        foreach(var q in queries)
+        {
+            Console.WriteLine(q);
+        }
+        Console.WriteLine("New Query ");
+        var MethodSyntax = intList.Where(i => i > 5 && i < 11).ToList();
+        foreach(var method in MethodSyntax)
+        {
+            Console.WriteLine(method);
+        }
     }
         
 }
