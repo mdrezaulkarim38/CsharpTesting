cbea5b6267dbe246dde2a94fbe995da23766eb79
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2024-12-02 08:14:28 +0600

    Testing code in Fleet

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,24 +1,9 @@
 ﻿namespace MyApp;
-public class Program
+class Program
 {
-    public static void Main(string[] args)
+    static void Main(string[] args)
     {
-        Car car = new Car();
-        car.Break();
-        car.SlowDown();
-        Console.WriteLine("Testing the code");
-    }
-}
-
-public class Car
-{
-    public string _Name = "Shuvo";
-    private int _Speed = 23;
-    int _Weight = 44;
-    public void Break(){
-        Console.WriteLine("Break "+ _Speed);
-    }
-    public void SlowDown(){
-        Console.WriteLine("Name & Weight "+ _Name + ""+ _Weight);
+        Console.WriteLine("Welcome To C#.Net");
+        Console.WriteLine("This is testing Auto Save");
     }
 }
