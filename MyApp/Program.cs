c26e54e3b78dcfc4bdd7c483c404096aeabeace5
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2024-12-09 21:48:16 +0600

    User define function

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -3,9 +3,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        string str1 = "Dot Net Tutorials";
-        string str2 = @"Dot Net Tutorials";
-        Console.WriteLine($"str1: {str1}");
-        Console.WriteLine($"str2: {str2}");
+        int x = 20, y = 10;
+        int result = Sum(x, y);
+        Console.WriteLine($"Sum is {result}");
+    }
+    static int Sum(int x, int y)
+    {
+        int sum = x + y;
+        return sum;
     }
 }
