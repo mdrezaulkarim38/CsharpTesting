d6c8e1b6bcf1fd464af2252142c97d6e147b1bf5
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2024-12-30 20:17:20 +0600

    Add some file

---
+++ api/Models/Comment.cs
@@ -0,0 +1,11 @@
+namespace api.Models;
+
+public class Comment
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    public string? Content { get; set; }
+    public DateTime CreatedOn { get; set; }
+    public int? StockId { get; set; }
+    public Stock? Stock { get; set; }
+}
