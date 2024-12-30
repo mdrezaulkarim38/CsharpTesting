f981a77b4cd3474ebf9a559b3912d68d23ddcd0c
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2024-12-30 20:22:22 +0600

    create connection string

--- api/Program.cs
+++ api/Program.cs
@@ -1,7 +1,15 @@
+using api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    {
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    });
 
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
