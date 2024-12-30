d68bd600736bf4941989e5c1d5b2bacbf8176c7b
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2024-12-30 21:27:45 +0600

    Stock Dto and Mappers Created

---
+++ api/Mappers/StockMappers.cs
@@ -0,0 +1,20 @@
+using api.Models;
+using api.Dtos.Stock;
+
+namespace api.Mappers;
+public static class StockMappers
+{
+    public static StockDto ToStockDto(this Stock stockModel)
+    {
+        return new StockDto
+        {
+            Id = stockModel.Id,
+            Symbol = stockModel.Symbol,
+            CompanyName = stockModel.CompanyName,
+            Purchase = stockModel.Purchase,
+            LastDiv = stockModel.LastDiv,
+            Industry = stockModel.Industry,
+            MarketCap = stockModel.MarketCap
+        };
+    }
+}
