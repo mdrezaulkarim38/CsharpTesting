8cfc88969408d866c41335cbb093ba7656407275
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2024-12-31 21:18:36 +0600

    Post and Put is done

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -1,4 +1,6 @@
 using api.Data;
+using api.Mappers;
+using api.Dtos.Stock;
 using Microsoft.AspNetCore.Mvc;
 namespace api.Controllers;
 
@@ -14,7 +16,7 @@ public class StockController : ControllerBase
     [HttpGet]
     public IActionResult GetAll()
     {
-        var stocks = _context.Stocks.ToList();
+        var stocks = _context.Stocks.ToList().Select(s => s.ToStockDto());
         return Ok(stocks);
     }
 
@@ -26,7 +28,35 @@ public class StockController : ControllerBase
         {
             return NotFound();
         }
-        return Ok(stock);
+        return Ok(stock.ToStockDto());
     }
 
+    [HttpPost]
+    public IActionResult Create([FromBody] CreateStockRequestDto stockDto)
+    {
+        var stockModel = stockDto.ToStockFromCreateDTO();
+        _context.Stocks.Add(stockModel);
+        _context.SaveChanges();
+        return CreatedAtAction(nameof(GetById), new { id = stockModel.Id}, stockModel.ToStockDto());
+    }
+
+    [HttpPut]
+    [Route("{id}")]
+    public IActionResult Update([FromRoute] int id, [FromBody] UpdateStockRequestDto updateDto)
+    {
+        var stockModel = _context.Stocks.FirstOrDefault(x => x.Id == id);
+        if(stockModel == null)
+        {
+            return NotFound();
+        }
+        stockModel.Symbol = updateDto.Symbol;
+        stockModel.CompanyName = updateDto.CompanyName;
+        stockModel.Purchase = updateDto.Purchase;
+        stockModel.LastDiv = updateDto.LastDiv;
+        stockModel.Industry = updateDto.Industry;
+        stockModel.MarketCap = updateDto.MarketCap;
+
+        _context.SaveChanges();
+        return Ok(stockModel.ToStockDto());
+    }
 }
--- api/Mappers/StockMappers.cs
+++ api/Mappers/StockMappers.cs
@@ -17,4 +17,18 @@ public static class StockMappers
             MarketCap = stockModel.MarketCap
         };
     }
+
+    public static Stock ToStockFromCreateDTO(this CreateStockRequestDto stockDto)
+    {
+        return new Stock
+        {
+            Symbol = stockDto.Symbol,
+            CompanyName = stockDto.CompanyName,
+            Purchase = stockDto.Purchase,
+            LastDiv = stockDto.LastDiv,
+            Industry = stockDto.Industry,
+            MarketCap = stockDto.MarketCap
+        };
+    }
+    
 }
