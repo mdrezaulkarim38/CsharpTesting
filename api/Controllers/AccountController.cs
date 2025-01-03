a0697dd138a7cd9240f927fbddecbbd4ce5318da
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-01-03 13:25:31 +0600

    now login register is working

--- api/Controllers/AccountController.cs
+++ api/Controllers/AccountController.cs
@@ -1,7 +1,10 @@
 using api.Models;
 using api.Dtos.Account;
+using api.Interfaces;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 namespace api.Controllers;
 
 [Route("api/account")]
@@ -9,11 +12,39 @@ namespace api.Controllers;
 public class AccountController : ControllerBase
 {
     private readonly UserManager<AppUser> _userManager;
-    public AccountController(UserManager<AppUser> userManager)
+    private readonly ITokenService _tokenService;
+    private readonly SignInManager<AppUser> _signInManager;
+    public AccountController(UserManager<AppUser> userManager, ITokenService iTokenService, SignInManager<AppUser> signInManager)
     {
         _userManager = userManager;
+        _tokenService = iTokenService;
+        _signInManager = signInManager;
     }
 
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
+    {
+        if(!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+        var user = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == loginDto.Username!.ToLower());
+        if(user == null)
+        {
+            return Unauthorized("Invalid username");
+        }
+        var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password!, false);
+        if(!result.Succeeded) return Unauthorized("UserName not found or password is invalid");
+
+        return Ok(
+            new NewUserDto
+            {
+                UserName = user.UserName,
+                Email = user.Email,
+                Token = _tokenService.CreateToken(user)
+            }
+            );
+    }
     [HttpPost("register")]
     public async Task<IActionResult> Register([FromBody] RegisterDto registerDto)
     {
@@ -34,7 +65,14 @@ public class AccountController : ControllerBase
                 var roleResult = await _userManager.AddToRoleAsync(appUser, "User");
                 if(roleResult.Succeeded)
                 {
-                    return Ok("User Created");
+                    return Ok(
+                    new NewUserDto
+                    {
+                        UserName = appUser.UserName,
+                        Email =  appUser.Email,
+                        Token = _tokenService.CreateToken(appUser)
+                    }
+                        );
                 }else
                 {
                     return StatusCode(500, roleResult.Errors);
---
+++ api/Interfaces/ITokenService.cs
@@ -0,0 +1,7 @@
+using api.Models;
+
+namespace api.Interfaces;
+public interface ITokenService
+{
+    string CreateToken(AppUser user);
+}
