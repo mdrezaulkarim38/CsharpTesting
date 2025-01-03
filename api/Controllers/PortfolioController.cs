d5d6ffadaa251ed92d00ea3332c79685b43c40ff
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-01-03 20:42:17 +0600

    create is done

--- api/Controllers/PortfolioController.cs
+++ api/Controllers/PortfolioController.cs
@@ -30,4 +30,36 @@ public class PortfolioController : Controller
         var userPortfolio = await _portfolioRepository.GetUserPortfolio(appUser!);
         return Ok(userPortfolio);
     }
+
+    [HttpPost]
+    [Authorize]
+    public async Task<IActionResult> AddPortfolio(string symbol)
+    {
+        var username = User.GetUsername();
+        var appUser = await _userManager.FindByNameAsync(username);
+        var stock = await _stockRepository.GetBySymbolAsync(symbol);
+        if(stock == null)
+        {
+            return BadRequest("Stock not Found");
+        }
+        var userPortfolio = await  _portfolioRepository.GetUserPortfolio(appUser);
+        if(userPortfolio.Any(e => e.Symbol!.ToLower() == symbol.ToLower()))
+        {
+            return BadRequest("Can not  add same stock to portfolio");
+        }
+        var portfolioModel = new Portfolio
+        {
+          StockId  = stock.Id,
+          AppUserId = appUser.Id
+        };
+
+        await _portfolioRepository.CreateAsync(portfolioModel);
+        if (portfolioModel == null)
+        {
+            return StatusCode(500, "Could not create");
+        }else
+        {
+            return Created();
+        }
+    }
 }
--- api/Repository/StockRepository.cs
+++ api/Repository/StockRepository.cs
@@ -81,5 +81,10 @@ public class StockRepository : IStockRepository
     {
         return _context.Stocks.AnyAsync(s => s.Id == id);
     }
+
+    public async Task<Stock?> GetBySymbolAsync(string symbol)
+    {
+        return await _context.Stocks.FirstOrDefaultAsync(s=> s.Symbol == symbol);
+    }
 }
 
