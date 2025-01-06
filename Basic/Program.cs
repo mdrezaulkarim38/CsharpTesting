84c04afb36e28dcbeba05f6e4d41ec24c8e8d485
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-01-06 22:14:15 +0600

    Add some bad code

--- Basic/Program.cs
+++ Basic/Program.cs
@@ -2,8 +2,16 @@
 
 public class Program
 {
+    static List<SomeClass> x = new List<SomeClass>();
     static void Main(string[] args)
     {
         Console.WriteLine("This is testing");
+        Console.WriteLine("Press Enter to start");
+        Console.Read();
+        for(double i = 0; i < 100000000000; i++)
+        {
+            SomeClass x = new SomeClass();
+            x.SomeData = DateTime.Now.ToString();
+        }
     }
 }
