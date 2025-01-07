223d4ed72ca9184abc67992afab2801ed25390d2
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-01-07 07:42:12 +0600

    Testing code

--- Basic/Program.cs
+++ Basic/Program.cs
@@ -1,17 +1,11 @@
-﻿namespace Basic;
+﻿using System;
+namespace Basic;
 
 public class Program
 {
     static List<SomeClass> x = new List<SomeClass>();
     static void Main(string[] args)
     {
-        Console.WriteLine("This is testing");
-        Console.WriteLine("Press Enter to start");
-        Console.Read();
-        for(double i = 0; i < 100000000000; i++)
-        {
-            SomeClass x = new SomeClass();
-            x.SomeData = DateTime.Now.ToString();
-        }
+        
     }
 }
