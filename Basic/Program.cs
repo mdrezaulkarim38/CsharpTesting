ed923aedc0d3de2689933cfdd44a99d45c962103
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-01-09 07:34:57 +0600

    casting

--- Basic/Program.cs
+++ Basic/Program.cs
@@ -5,6 +5,9 @@ public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("This is testing");
+        int  i = 10;
+        double d2 = i; // casting (implicit casting)
+        double d1 = 3;
+        int y = (int) d1; // explicit casting 
     }
 }
