fc313b7f2516955efdce829fab37f1cf0586792d
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-01-09 08:01:07 +0600

    Out keyword use

--- Basic/Program.cs
+++ Basic/Program.cs
@@ -11,7 +11,7 @@ public class Program
         //        double d2 = i; // casting (implicit casting)
         //        double d1 = 3;
         //        int y = (int) d1; // explicit casting
-        int[] marray = {1, 2, 3}; // array (fixed, strongly typed)
+        /*int[] marray = {1, 2, 3}; // array (fixed, strongly typed)
         ArrayList mylist = new ArrayList();// arraylist (flexible)
         mylist.Add(1);
         mylist.Add(3);
@@ -19,8 +19,8 @@ public class Program
         // strongly type, size is flexible
         List<int> geneint = new List<int>();
         geneint.Add(1);
-        geneint.Add(2);
-        try
+        geneint.Add(2);*/
+       /* try
         {
             int y = 0;
             int x = 1 / y;
@@ -32,6 +32,16 @@ public class Program
         finally
         {
             Console.WriteLine("Clean up code");
-        }
+        }*/
+       int add = 0;
+       int sub = 0;
+       MyMaths(10,10,out add, out sub);
+       Console.WriteLine("The addition is: "+add);
+       Console.WriteLine("The subtraction is: "+sub);
+    }
+    static void MyMaths(int num1, int num2, out int add, out int sub) // If you want to return mulitple outputs from a function you can use OUT Keyword
+    {
+        add = num1 + num2;
+        sub = num1 - num2;
     }
 }
