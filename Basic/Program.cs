856345cf8d9e9fc38378bd1f922b2e96e7b75214
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-01-10 21:07:57 +0600

    Polymorphism

--- Basic/Program.cs
+++ Basic/Program.cs
@@ -1,25 +1,41 @@
 ﻿using System;
 namespace Basic;
+
+public class Animal
+{
+    public virtual void AnimalSound()
+    {
+        Console.WriteLine("The animal makes a sound");
+    }
+}
+
+public class Pig : Animal
+{
+    public override void AnimalSound()
+    {
+        Console.WriteLine("The pig makes a sound: wee wee");
+    }
+}
+
+public class Dog : Animal
+{
+    public override void AnimalSound()
+    {
+        Console.WriteLine("The dog makes a sound: bow wow");
+    }
+}
+
 public class Program
 {
     static void Main(string[] args)
     {
-        Employee employee = new Employee();
+        Animal myAnimal = new Animal();
+        Animal pig = new Pig();
+        Animal myDog = new Dog();
+        
+        myAnimal.AnimalSound();
+        myDog.AnimalSound();
+        pig.AnimalSound();
         Console.WriteLine("Hello World!");
-    }
-
-    public class Employee
-    {
-        public string? Name { get; set; }
-        public string? Address { get; set; }
-        public string? City { get; set; }
-
-        public void Validate()
-        {
-            if (Name is null)
-            {
-                Console.WriteLine("Name is required");
-            }
-        }
     }
 }
