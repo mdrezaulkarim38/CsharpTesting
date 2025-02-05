48a62d453500b2c3d41cc6ee14aa979f194ca55e
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-02-05 22:36:31 +0600

    get the sum

--- Basic/Program.cs
+++ Basic/Program.cs
@@ -1,70 +1,21 @@
-﻿using System;
-namespace Basic;
+﻿namespace Basic;
 
-public class Animal
-{
-    public virtual void AnimalSound()
-    {
-        Console.WriteLine("The animal makes a sound");
-    }
-}
-
-public class Pig : Animal
-{
-    public override void AnimalSound()
-    {
-        Console.WriteLine("The pig makes a sound: wee wee");
-    }
-}
-
-public class Dog : Animal
-{
-    public override void AnimalSound()
-    {
-        Console.WriteLine("The dog makes a sound: bow wow");
-    }
-} 
-
-public class SomeClass
-{
-    private int someValue;
-
-    public SomeClass(int val)
-    {
-        someValue = val;
-    }
-    public static SomeClass operator +(SomeClass arg1, SomeClass arg2)
-    {
-        return new SomeClass(arg1.someValue + arg2.someValue);
-    }
-}
 
 public class Program
 {
-    static int LongestSubstring(string s)
+    public  static void Main(string[] args)
     {
-        int n = s.Length;
-        int maxLength = 0;
-        int start = 0;
-        HashSet<char> seenChar = new HashSet<char>();
-        for (int end = 0; end < n; end++)
+        int n = Convert.ToInt32(Console.ReadLine());
+        // int sum = ((n * (n + 1)) / 2);
+        int sum = 0;
+        if(n % 2 == 0)
         {
-            while (seenChar.Contains(s[end]))
-            {
-                seenChar.Remove(s[start]);
-                start++;
-            }
-
-            seenChar.Add(s[end]);
-            maxLength = Math.Max(maxLength, end - start + 1);
+            sum = (n / 2) * (n + 1);
         }
-
-        return maxLength;
-    }
-    static void Main(string[] args)
-    {
-        Console.WriteLine(LongestSubstring("geeksforgeeks")); // Output: 7
-        Console.WriteLine(LongestSubstring("aaa")); // Output: 1
-        Console.WriteLine(LongestSubstring("abcdefabcbb"));
+        else
+        {
+            sum = ((n + 1) / 2) * n;
+        }
+        Console.WriteLine(sum);
     }
 }
--- Basic/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Basic;
-
-public class User
-{
-    public int Id { get; set; }
-    public string? UserId { get; set; }
-    public string? UserName { get; set; }
-    public string? Password { get; set; } 
-}
