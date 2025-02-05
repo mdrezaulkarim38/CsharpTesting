023d23ee2f0806c31224c368d8ac216aaa9cdb97
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-02-05 22:45:54 +0600

    done

--- Basic/Program.cs
+++ Basic/Program.cs
@@ -5,17 +5,11 @@ public class Program
 {
     public  static void Main(string[] args)
     {
-        int n = Convert.ToInt32(Console.ReadLine());
-        // int sum = ((n * (n + 1)) / 2);
-        int sum = 0;
-        if(n % 2 == 0)
-        {
-            sum = (n / 2) * (n + 1);
-        }
-        else
-        {
-            sum = ((n + 1) / 2) * n;
-        }
-        Console.WriteLine(sum);
+        int a = Convert.ToInt32(Console.ReadLine());
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        (a, b) = (b , a);
+        Console.WriteLine(a);
+        Console.WriteLine(b);
     }
 }
