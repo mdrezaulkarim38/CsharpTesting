203a54740c4dba53cfca077b4841237ae5898df2
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-03-07 20:50:08 +0600

    Method overloading

--- Basic/Program.cs
+++ Basic/Program.cs
@@ -7,12 +7,18 @@ public class Program
         x = 10;
         y = 20;
         int sum = Add(x, y);
+        int total = Add(x, y, sum);
         Console.WriteLine(sum);
+        Console.WriteLine(total);
     }
 
     static int Add(int a, int b)
     {
         int sum = a + b;
         return sum;
+    }
+    static int Add(int x, int y, int z)
+    {
+        return x + y + z;
     }
 }
