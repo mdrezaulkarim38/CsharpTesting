dc32f45d2d35a97f29eb9b9df290c594eb2f4f2a
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-03-07 20:42:56 +0600

    testing Main

--- Basic/Program.cs
+++ Basic/Program.cs
@@ -3,13 +3,9 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("1st Statement: ");
-        goto label1;
-        Console.WriteLine("2nd Statement: ");
-        label1:
-            Console.WriteLine("3rd statement: ");
-
-        Console.WriteLine("End of Main Method Statement");
+        int number = 25;
+        double squareRoot = Math.Sqrt(number);
+        Console.WriteLine(squareRoot);
     
     }
 }
