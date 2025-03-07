2bffe4f056e30fa9da4976724da07a0af18d4f49
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-03-07 20:42:03 +0600

    testing goto class

--- Basic/Program.cs
+++ Basic/Program.cs
@@ -3,12 +3,13 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        List<string> nameList = new List<string>();
-        nameList.Add("shuvo");
-        nameList.Add("Superman");
-        foreach(string name in nameList)
-        {
-            Console.WriteLine(name);
-        }
+        Console.WriteLine("1st Statement: ");
+        goto label1;
+        Console.WriteLine("2nd Statement: ");
+        label1:
+            Console.WriteLine("3rd statement: ");
+
+        Console.WriteLine("End of Main Method Statement");
+    
     }
 }
