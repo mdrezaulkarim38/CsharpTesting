3441ab05aa6b9a44c79eaaabaf6b3da92807a948
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-03-07 20:45:14 +0600

    add static method

--- Basic/Program.cs
+++ Basic/Program.cs
@@ -3,9 +3,16 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        int number = 25;
-        double squareRoot = Math.Sqrt(number);
-        Console.WriteLine(squareRoot);
-    
+        int x, y;
+        x = 10;
+        y = 20;
+        int sum = Add(x, y);
+        Console.WriteLine(sum);
+    }
+
+    static int Add(int a, int b)
+    {
+        int sum = a + b;
+        return sum;
     }
 }
