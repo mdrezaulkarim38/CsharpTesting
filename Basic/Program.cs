01c3ac9c529b873c44864f9870f3febdff8c4b60
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-03-08 09:55:47 +0600

    Input code is add

--- Basic/Program.cs
+++ Basic/Program.cs
@@ -3,17 +3,9 @@ public class Program
 {
     static void Main(string[] args)
     {
-        int x = 5;
-        Console.WriteLine($"The factorial of {x} is {Fun(x)}");
-    }
-
-    static int Fun(int n)
-    {
-        int result  = 0;
-        for(int i = 1; i <= n; i++)
-        {
-            result = result + 1;
-        }
-        return result;
+        string str;
+        Console.WriteLine("Enter a string - ");
+        str = Console.ReadLine();
+        Console.WriteLine($"You Enter {str}");
     }
 }
