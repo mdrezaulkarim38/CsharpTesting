9e614caf880870718721f27c24a1821eaff30f3c
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-03-08 09:53:51 +0600

    Update code

--- Basic/Program.cs
+++ Basic/Program.cs
@@ -4,18 +4,16 @@ public class Program
     static void Main(string[] args)
     {
         int x = 5;
-        Console.WriteLine($"The factorial of {x} is {factorial(x)}");
+        Console.WriteLine($"The factorial of {x} is {Fun(x)}");
     }
 
-    static int factorial(int number)
+    static int Fun(int n)
     {
-        if(number == 1)
+        int result  = 0;
+        for(int i = 1; i <= n; i++)
         {
-            return (1);
+            result = result + 1;
         }
-        else
-        {
-            return (number * factorial(number - 1));
-        }
+        return result;
     }
 }
