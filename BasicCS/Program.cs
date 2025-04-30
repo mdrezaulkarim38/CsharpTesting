5002541696c96f49f5b79ed07c1c72af9d0647a5
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-04-30 06:51:59 +0600

    need to learn

--- BasicCS/Program.cs
+++ BasicCS/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace BasicCS;
+
+public class Program
+{
+    static List<int> findTwoElements(int[] arr)
+    {
+        int n = arr.Length;
+        int[] freq = new int[n + 1];
+        int repeting = -1;
+        int missing = -1;
+        for (int i = 0; i < n; i++)
+        {
+            freq[arr[i]]++;
+        }
+
+        for (int i = 0; i <= n; i++)
+        {
+            if (freq[i] == 0)
+            {
+                missing = i;
+            }
+            else if (freq[i] == 2)
+            {
+                repeting = i;
+            }
+        }
+        
+        return new List<int> { repeting, missing };
+    }
+
+    static void Main()
+    {
+        int[] arr = { 3, 1, 3 };
+        List<int> ans = findTwoElements(arr);
+        Console.WriteLine(ans[0] + " " + ans[1]);
+    }
+}
