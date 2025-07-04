7a09907bf3755c8f43c3827aed75307ae650111f
MD Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-07-04 21:14:59 +0600

    need to add text

---
+++ BasicLearn/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace BasicLearn;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int i = 1;
+        int I = 0;
+        Console.WriteLine(i);
+        Console.WriteLine("Hello");
+    }
+}
