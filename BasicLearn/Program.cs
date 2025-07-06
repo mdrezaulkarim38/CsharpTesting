a08583b50380bfc1cef628644d43e329e7ef1bf7
MD Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-07-06 09:47:37 +0600

    testing logical program

--- BasicLearn/Program.cs
+++ BasicLearn/Program.cs
@@ -5,22 +5,6 @@ public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Enter userName");
-        string userName = Console.ReadLine();
-        Console.WriteLine("User Name is: " + userName);
-        Car Ford = new();
-        Console.WriteLine($"Car {Ford.model}");
+        
     }
-}
-
-public class Patient
-{
-    public string Name { get; set; }
-    public string Address { get; set; }
-    public Doctor Doctor{ get; set; }
-}
-
-public class Doctor
-{
-    public string Name { get; set;}
 }
