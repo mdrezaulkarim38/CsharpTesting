d362e1c9175e762ca45f15d5b7c3df29f552d3a6
MD Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-07-06 09:53:08 +0600

    basic swap

--- BasicLearn/Program.cs
+++ BasicLearn/Program.cs
@@ -5,6 +5,11 @@ public class Program
 {
     static void Main(string[] args)
     {
-        
+        int number1 = 10, number2 = 20;
+        Console.WriteLine(number1 + " before " + number2);
+        int temp = number1;
+        number1 = number2;
+        number2 = temp;
+        Console.WriteLine(number1 + " after " + number2);
     }
 }
