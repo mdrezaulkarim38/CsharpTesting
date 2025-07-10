8d4d53111c36c5f228834fd567a5f835f23b064b
MD Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-07-10 21:44:39 +0600

    constructor

--- BasicLearn/Program.cs
+++ BasicLearn/Program.cs
@@ -1,17 +1,26 @@
 ﻿using System;
 namespace BasicLearn;
 
-
+class sample
+{
+    public sample()
+    {
+        Console.WriteLine("constructor 1 called");
+    }
+    public sample(int x)
+    {
+        int p = 2;
+        int u;
+        u = p + x;
+        Console.WriteLine("constructor 2 called");
+    }
+}
 class Program
 {
     static void Main(string[] args)
     {
-        int a = 5;
-        fun1(ref a);
-        Console.WriteLine(a);
-    }
-    static void fun1(ref int a)
-    {
-        a = a * a;
+        sample s = new sample(4);
+        sample t = new sample();
+    
     }
 }
