692c739245dd8994d4aab2b8416794b44f12f125
MD Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-07-10 22:42:39 +0600

    done

--- BasicLearn/Program.cs
+++ BasicLearn/Program.cs
@@ -24,3 +24,38 @@ class Program
     
     }
 }
+
+/**
+In Angular,  Will both components share the same count?
+
+@Injectable ({ providedIn: 'root'})
+export class CounterService {
+    count = 0;
+    increment() {this.count++;}
+}
+
+@Component({
+    selector: 'app-a',
+    template: `<button (click)="add()">add</button>`
+})
+
+export class AComponent {
+    constructor(public cs: CounterService) {}
+    add() { this.cs.increment(); }
+}
+
+@Component({
+    selector: 'app-b',
+    template: `<p>{{ cs.count }}</p>`
+})
+
+export class BComponent{
+    constructor(public cs: CounterService) {}
+}
+
+Will not compile
+No, separate instances
+Yes, shared instance
+Static count
+
+*/
