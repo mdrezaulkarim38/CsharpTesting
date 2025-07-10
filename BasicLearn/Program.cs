360e7e338c986a8eea325b5afacca6fc53f1de2f
MD Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-07-10 21:35:57 +0600

    another error

--- BasicLearn/Program.cs
+++ BasicLearn/Program.cs
@@ -1,41 +1,10 @@
 ﻿using System;
 namespace BasicLearn;
-
-public class Calculation
-{
-    int sum = 0;
-    int count = 0;
-    float average;
-    public void CalAverage()
-    {
-        if (count == 0)
-        throw (new CountIsZeroException("Zero count in DoAverage"));
-        {
-            average = (float)sum / count;
-            Console.WriteLine("Program executed successfully");
-        }
-    }
-}
-
-public class CountIsZeroException : ApplicationException
-{
-    public CountIsZeroException(string message) : base(message)
-    { }
-}
-
 class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        Calculation c = new Calculation();
-        try
-        {
-            c.CalAverage();
-        }
-        catch (CountIsZeroException e)
-        {
-            Console.WriteLine("Count is ZeroException: {0}", e.Message);
-        }
-        Console.WriteLine("");
+        int[] i = new int[0];
+        Console.WriteLine(i[0]); 
     }
 }
