4cb7ac090db1b9af0d3762f343c1ef375ada5d47
MD Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-07-20 22:15:21 +0600

    linklist

--- CoreLearning/Program.cs
+++ CoreLearning/Program.cs
@@ -1,3 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Hello, World!");
+public class Node {
+    public int Data;
+    public Node Next;
+
+    public Node(int data) {
+        this.Data = data;
+        this.Next = null;
+    }
+}
+
+public class LinkedList {
+    private Node head;
+
+    public void Add(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node temp = head;
+        while (temp.Next != null)
+            temp = temp.Next;
+
+        temp.Next = newNode;
+    }
+
+    public void Print() {
+        Node temp = head;
+        while (temp != null) {
+            Console.Write(temp.Data + " -> ");
+            temp = temp.Next;
+        }
+        Console.WriteLine("null");
+    }
+}
+
+class Program {
+    static void Main() {
+        LinkedList list = new LinkedList();
+        list.Add(10);
+        list.Add(20);
+        list.Add(30);
+        list.Add(50);
+        list.Print();
+    }
+}
