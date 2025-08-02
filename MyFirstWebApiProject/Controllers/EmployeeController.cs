a210db1717fadab25a37e76a4456604ad2ab7577
MD Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-08-02 17:09:21 +0600

    new controller

---
+++ MyFirstWebApiProject/Controllers/EmployeeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstWebApiProject.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class EmployeeController : ControllerBase
+{
+    [HttpGet]
+    public string Get()
+    {
+        return "Returning From EmployeeController";
+    }
+}
---
+++ MyFirstWebApiProject/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyFirstWebApiProject.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    [Required]
+    [StringLength(100, MinimumLength = 3)]
+    public string? Name { get; set; }
+    
+    [Range(0.01, 10000.00)]
+    public decimal Price { get; set; }
+    
+    [Required]
+    public string? Category { get; set; }
+}
