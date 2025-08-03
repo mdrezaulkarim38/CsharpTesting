dde120c41b16e9065a7ea456f93f1c3ba9b2b759
MD Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-08-03 22:06:12 +0600

    testing

--- CoreLearning/Program.cs
+++ CoreLearning/Program.cs
@@ -4,5 +4,6 @@ public static class Program
     public static void Main(string[] args)
     {
         Console.WriteLine("Hello Windows");
+        Console.WriteLine("I am a software developer");
     }
 }
