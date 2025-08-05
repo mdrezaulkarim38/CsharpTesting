a540e3ffbe1bab0e46f8aeeaca5a34cec9dc43af
MD Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-08-05 13:08:19 +0600

    Testing

---
+++ MyFirstWebApiProject/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace MyFirstWebApiProject.Models;
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Gender { get; set; }
+    public string Department { get; set; }
+    public string City { get; set; }
+}
