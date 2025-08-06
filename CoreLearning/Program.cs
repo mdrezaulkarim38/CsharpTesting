ee1c20ab1a9404ab91a7d55f01d085b903632439
MD Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-08-06 21:37:41 +0600

    check number

--- CoreLearning/Program.cs
+++ CoreLearning/Program.cs
@@ -4,18 +4,24 @@ public static class Program
 {
     public static void Main(string[] args)
     {
-        char letter;
-        Console.WriteLine("Enter any letter: ");
-        letter = Convert.ToChar(Console.ReadLine());
-        letter = char.ToLower(letter);
-        if (letter == 'a' || letter == 'i' || letter == 'o' || letter == 'e'  || letter == 'u')
+        int digit;
+        Console.Write("Enter a number: ");
+        digit = Convert.ToInt32(Console.ReadLine());
+        switch (digit)
         {
-            Console.WriteLine("You entered vowel character."+letter);
+            case 1:
+                Console.Write("You Entered one ");
+                break;
+            case 2:
+                Console.Write("You Entered two ");
+                break;
+            case 3:
+                Console.Write("You Entered three ");
+                break;
+            default:
+                Console.Write("You Entered greater then one two three ");
+                break;
         }
-        else
-        {
-            Console.WriteLine("You entered consonant character."+letter);
-        }
-
+        
     }
 }
