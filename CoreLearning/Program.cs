be0c42fc096452f0111e953ece03a82e2aa91681
MD Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-08-07 22:06:39 +0600

    Solve Problem

--- CoreLearning/Program.cs
+++ CoreLearning/Program.cs
@@ -22,13 +22,35 @@
 
             return max;
         }
+
+        static void bubbleSort(int[] arr, int n){
+            int i, j, temp;
+            bool swapped;
+            for (i = 0; i < n - 1; i++) {
+                swapped = false;
+                for (j = 0; j < n - i - 1; j++) {
+                    if (arr[j] > arr[j + 1]) {
+                        temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                        swapped = true;
+                    }
+                }
+                if (swapped == false)
+                    break;
+            }
+        }
+        static int KthSmallest(int[] arr, int k)
+        {
+            bubbleSort(arr, arr.Length);
+            return arr[k-1];
+        }
         static void Main(string[] args)
         {
-            int[] A = { 4, 9, 6, 5, 2, 3 };
-            int N = 6;
+            int[] arr = [2, 3, 1, 20, 15];
+            int k = 4;
+            Console.WriteLine(KthSmallest(arr, k));
             
-            Console.WriteLine(Setmini(A, N));
-            Console.WriteLine(SetMax(A, N));
         }
     }
 }
