9c145902fd0811c102c4ff1e7cd934ed00f7b97c
MD Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-08-08 18:24:50 +0600

    Encapsulations

--- CoreLearning/Program.cs
+++ CoreLearning/Program.cs
@@ -6,5 +6,5 @@ BankAccount _bankAccount = new BankAccount(100);
 Console.WriteLine(_bankAccount.GetBalance());
 _bankAccount.Deposit(50);
 Console.WriteLine(_bankAccount.GetBalance());
-_bankAccount.Withdraw(20);
+_bankAccount.Withdraw(-20);
 Console.WriteLine(_bankAccount.GetBalance());
