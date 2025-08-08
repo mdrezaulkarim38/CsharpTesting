63c496e81760d0e809a79248d2b91a14e36f904e
MD Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-08-08 18:20:36 +0600

    Now get the balance

--- CoreLearning/OopPrinciples/Encapsulation/BankAccount.cs
+++ CoreLearning/OopPrinciples/Encapsulation/BankAccount.cs
@@ -2,5 +2,38 @@
 
 public class BankAccount
 {
-    
+    private decimal _balance;
+
+    public BankAccount(decimal balance)
+    {
+        Deposit(balance);
+    }
+
+    public decimal GetBalance()
+    {
+        return _balance;
+    }
+
+    public void Deposit(decimal amount)
+    {
+        if (amount <= 0)
+        {
+            throw new ArgumentException("Deposit amount must be greater than zero");
+        }
+        this._balance += amount;
+    }
+
+    public void Withdraw(decimal amount)
+    {
+        if (amount <= 0)
+        {
+            throw new ArgumentException("Withdraw amount must be greater than zero");
+        }
+
+        if (amount > this._balance)
+        {
+            throw new InvalidOperationException("Insufficient balance");
+        }
+        this._balance -= amount;
+    }
 }
