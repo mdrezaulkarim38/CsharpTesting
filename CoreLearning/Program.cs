be2860b22c9fa29153f968174d97b795b417d738
MD Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-08-20 21:41:18 +0600

    testing basic

--- CoreLearning/Program.cs
+++ CoreLearning/Program.cs
@@ -1,13 +1,13 @@
-﻿using CoreLearning.OopPrinciples.Encapsulation;
-
-BankAccount bankAccount = new BankAccount(100);
-Console.WriteLine(bankAccount.GetBalance());
-bankAccount.Deposit(50);
-Console.WriteLine(bankAccount.GetBalance());
-bankAccount.Withdraw(25);
-Console.WriteLine(bankAccount.GetBalance());
-// bankAccount.Withdraw(200);
-// Console.WriteLine(bankAccount.GetBalance());
-
-int n = 0;
-Console.WriteLine(n++ + ++n);
+﻿ushort num1 = 123;
+            
+//UInt32 num2 = 456;
+uint num2 = 456;
+// UInt64 num3 = 789;
+ulong num3 = 789;
+Console.WriteLine($"ushort Min Value:{ushort.MinValue} and Max Value:{ushort.MaxValue}");
+Console.WriteLine($"short Size:{sizeof(ushort)} Byte");
+Console.WriteLine($"uint Min Value:{uint.MinValue} and Max Value:{uint.MaxValue}");
+Console.WriteLine($"uint Size:{sizeof(uint)} Byte");
+Console.WriteLine($"ulong Min Value:{ulong.MinValue} and Max Value:{ulong.MaxValue}");
+Console.WriteLine($"ulong Size:{sizeof(ulong)} Byte");
+Console.ReadKey();
