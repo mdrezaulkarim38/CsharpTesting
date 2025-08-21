c642573465d1c8f5d79932eab1843a755e443c87
MD Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-08-21 07:52:02 +0600

    make simple change

--- CoreLearning/Program.cs
+++ CoreLearning/Program.cs
@@ -1,9 +1,2 @@
-﻿int a = 101;
-int c = 0x123f;
-int d = 0b1111;
-Console.WriteLine($"Decimal Literal: {a}");
-Console.WriteLine($"Hexa-Decimal Literal: {c}");
-Console.WriteLine($"Binary Literal: {d}");
-
-int  s = 0;
-Console.WriteLine(s);
+﻿int age = 56;
+Console.WriteLine($"Age: {age}");
