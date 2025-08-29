3dd66d35f5d92e24351ffa6a7270bb4c9cc6b476
MD Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-08-29 19:49:59 +0600

    testing code

--- CoreLearning/Program.cs
+++ CoreLearning/Program.cs
@@ -1,2 +1,18 @@
-﻿int age = 56;
-Console.WriteLine($"Age: {age}");
+﻿using System.Collections;
+
+namespace CoreLearning;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        ArrayList list = new ArrayList();
+        list.Add(23);
+        list.Add("Shuvo");
+        List<int> ages = new List<int>();
+        ages.Add(23);
+        ages.Add(24);
+        Console.WriteLine();
+        Console.WriteLine("Press enter to Shut down...");
+    }
+}
