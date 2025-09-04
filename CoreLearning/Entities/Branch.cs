9fd398643c5d713428bb110b02e432d6c409d420
MD Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-09-04 13:13:31 +0600

    Create Model

---
+++ CoreLearning/Entities/Branch.cs
@@ -0,0 +1,11 @@
+namespace CoreLearning.Entities;
+
+public class Branch
+{
+    public int BranchId { get; set; }
+    public string? BranchName { get; set; }
+    public string? Description { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? Email { get; set; }
+    
+}
