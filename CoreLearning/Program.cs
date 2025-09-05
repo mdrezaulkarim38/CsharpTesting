f9c4c6d45dcc6aa06e8f554120b4072b16c7cc9e
MD Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-09-05 08:24:17 +0600

    Min max little bit

--- CoreLearning/Program.cs
+++ CoreLearning/Program.cs
@@ -1,13 +1,40 @@
 ﻿using System;
 namespace CoreLearning;
 
+public struct Pair
+{
+    public int min;
+    public int max;
+}
+
 public class Program
 {
+    public static Pair GetMinMax(int[] arr, int n)
+    {
+        Pair minimax = new Pair();
+        Array.Sort(arr);
+        minimax.min = arr[0];
+        minimax.max = arr[n - 1];
+
+        return minimax;
+    }
     public static void reverseArray(int[] arr)
     {
         Array.Reverse(arr);
     }
 
+    public static void reverseArrayCustom(int[] arr)
+    {
+        int left = 0, right = arr.Length - 1;
+        while (left < right)
+        {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+    }
     static void Main()
     {
         int[] arr = { 1, 4, 3, 2, 6, 5 };
