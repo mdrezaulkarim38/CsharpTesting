5be916be9c562cfd5fa1f75916073b1e73fe4d7d
MD Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-09-05 08:15:19 +0600

    Array.Reverse array

--- CoreLearning/Program.cs
+++ CoreLearning/Program.cs
@@ -1,3 +1,20 @@
-﻿List<int> numbers = [1, 2, 3, 4];
-List<string> strings = ["Tim", "Corey", "Sue"];
+﻿using System;
+namespace CoreLearning;
 
+public class Program
+{
+    public static void reverseArray(int[] arr)
+    {
+        Array.Reverse(arr);
+    }
+
+    static void Main()
+    {
+        int[] arr = { 1, 4, 3, 2, 6, 5 };
+        reverseArray(arr);
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.Write(arr[i] + " ");
+        }
+    }
+}
