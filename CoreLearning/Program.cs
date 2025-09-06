e47eff0480846037194a798d3a378b1b6198afce
MD Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-09-06 11:53:33 +0600

    testing

--- CoreLearning/Program.cs
+++ CoreLearning/Program.cs
@@ -1,53 +1,20 @@
 ﻿using System;
 namespace CoreLearning;
 
-public struct Pair
-{
-    public int min;
-    public int max;
-}
-
 public class Program
 {
-    public static Pair GetMinMax(int[] arr, int n)
-    {
-        Pair minimax = new Pair();
-        Array.Sort(arr);
-        minimax.min = arr[0];
-        minimax.max = arr[n - 1];
-
-        return minimax;
-    }
-    public static void reverseArray(int[] arr)
-    {
-        Array.Reverse(arr);
-    }
-
-    public static void reverseArrayCustom(int[] arr)
-    {
-        int left = 0, right = arr.Length - 1;
-        while (left < right)
-        {
-            int temp = arr[left];
-            arr[left] = arr[right];
-            arr[right] = temp;
-            left++;
-            right--;
-        }
-    }
-    static void Print(string str)
-    {
-        Console.Write(str);
-    }
-    static void Println(string str)
-    {
-        Console.WriteLine(str);
-    }
     static void Main()
     {
-        bool isValid = true;
-        Println(isValid.ToString());
-        sbyte level = 127;
-        Println(level.ToString());
+        Calculator calObject = new Calculator();
+        int result = calObject.CalculateSum(10, 20);
+        Console.WriteLine(result);
+    }
+}
+
+public class Calculator
+{
+    public int CalculateSum(int no1, int no2)
+    {
+        return no1 + no2;
     }
 }
