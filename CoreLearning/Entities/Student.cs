5865ffa0e5e04eb3e359f0a6b5acb194bfb5b63c
MD Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-10-24 20:40:20 +0600

    Student Entities

---
+++ CoreLearning/Entities/Student.cs
@@ -0,0 +1,13 @@
+namespace CoreLearning.Entities;
+public class Student
+{
+    public int StudentId { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public DateTime? DateOfBirth { get; set; }
+    public string? Gender { get; set; }
+    public string? Email { get; set; }
+    public string? PhoneNumber { get; set; }
+    public DateTime EnrollmentDate { get; set; }
+    public virtual Branch? Branch { get; set; }
+}
